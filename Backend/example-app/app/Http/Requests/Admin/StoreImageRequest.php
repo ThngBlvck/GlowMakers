15714c0530dfc18c4efb9f52b60ba2be63f7c4bb
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-10-08 15:11:18 +0700

    bỏ bắt lỗi hình ảnh

--- Backend/example-app/app/Http/Requests/Admin/StoreImageRequest.php
+++ Backend/example-app/app/Http/Requests/Admin/StoreImageRequest.php
@@ -17,14 +17,13 @@ class StoreImageRequest extends FormRequest
     public function rules(): array
     {
         $rules = [
-            'product_id' => 'required|exists:products,id', // Kiểm tra ID sản phẩm có tồn tại
-            'images.*' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp', // Quy tắc cho từng ảnh
+            'product_id' => 'required|exists:products,id' // Kiểm tra ID sản phẩm có tồn tại
         ];
 
         if ($this->isMethod('put') || $this->isMethod('patch')) {
             $id = $this->route('image');
 
-            // Trong trường hợp sửa, bạn có thể thêm quy tắc khác nếu cần
+
         }
 
         return $rules;
@@ -35,8 +34,7 @@ class StoreImageRequest extends FormRequest
         return [
             'product_id.required' => 'ID sản phẩm là bắt buộc.',
             'product_id.exists' => 'Sản phẩm không tồn tại.',
-            'images.*.image' => 'Mỗi tệp phải là một hình ảnh.',
-            'images.*.mimes' => 'Hình ảnh phải có định dạng jpeg, png, jpg, gif, hoặc svg, webp.',
+
         ];
     }
 
