e1d3e7fd6e2a4a4d9263baa3207931990506723e
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-10-08 14:58:03 +0700

    Sửa services Brand, Comment. Route backend

--- Frontend/src/admin/index.js
+++ Frontend/src/admin/index.js
@@ -9,37 +9,36 @@ import Footer from "./components/Footer";
 
 // views
 import Dashboard from "./Pages/Dashboard";
-
-//Product
 import Product from "./Pages/Product/List";
-import ProductDetail from "./Pages/Product/ProductDetail";
+import ProductDetail from "./Pages//Product/ProductDetail";
 import AddProduct from "./Pages/Product/Add";
 import EditProduct from "./Pages/Product/Edit";
 
-//CategoryProduct
 import ProductCategory from "./Pages/ProductCategory/List";
-import AddProductCategory from "./Pages/ProductCategory/Add";
-import EditProductCaterogy from "./Pages/ProductCategory/Edit";
-
-import Blog from "./Pages/Blog";
+import Blog from "./Pages/Blog/List";
 import Brand from "./Pages/Brand/List";
-// import Comment from "./Pages/Comment/List";
+import Comment from "./Pages/Comment/List";
 import Role from "./Pages/Role/List";
 import Order from "./Pages/Order";
 import BlogCaterogy from "./Pages/BlogCategory/List";
+import AddProductCategory from "./Pages/ProductCategory/Add";
+import EditProductCaterogy from "./Pages/ProductCategory/Edit";
 
+import AddBrand from "./Pages/Brand/Add";
+import EditBrand from "./Pages/Brand/Edit";
 
 import AddBlogCategory from "./Pages/BlogCategory/Add";
 import EditBlogCategory from "./Pages/BlogCategory/Edit";
 
 
-import EditBrand from "./Pages/Brand/Edit";
-import AddBrand from "./Pages/Brand/Add";
-
 import AddRole from "./Pages/Role/Add";
 import EditRole from "./Pages/Role/Edit";
 
+import AddBlog from "./Pages/Blog/Add";
+import EditBlog from "./Pages/Blog/Edit";
 
+// import Settings from "views/admin/Settings.js";
+// import Tables from "views/admin/Tables.js";
 
 export default function Admin() {
     return (
@@ -58,20 +57,29 @@ export default function Admin() {
                         <Route path="category_product/edit/:id" element={<EditProductCaterogy />} />
 
                         <Route path="product" element={<Product />} />
-                        <Route path="product/detail/:id" element={<ProductDetail />} />
                         <Route path="product/add" element={<AddProduct />} />
                         <Route path="product/edit/:id" element={<EditProduct />} />
+                        <Route path="product/detail/:id" element={<ProductDetail />} />
 
                         {/*<Route path="user" element={<User />} />*/}
 
                         <Route path="category_blog" element={<BlogCaterogy />} />
                         <Route path="category_blog/add" element={<AddBlogCategory />} />
                         <Route path="category_blog/edit/:id" element={<EditBlogCategory />} />
+
                         <Route path="blog" element={<Blog />}/>
-                        <Route path="brand" element={<Brand/>}/>
-                        <Route path="brand/add" element={<AddBrand/>}/>
-                        <Route path="brand/edit/:id" element={<EditBrand/>}/>
-                        {/*<Route path="comment" element={<Comment />}/>*/}
+                        <Route path="blog/add" element={<AddBlog />}/>
+                        <Route path="blog/edit/:id" element={<EditBlog />}/>
+
+
+
+                        <Route path="brand" element={<Brand />}/>
+                        <Route path="brand/add" element={<AddBrand />}/>
+                        <Route path="brand/edit/:id" element={<EditBrand />}/>
+
+
+
+                        <Route path="comment" element={<Comment />}/>
                         <Route path="order" element={<Order />}/>
                         <Route path="role" element={<Role />}/>
                         <Route path="role/add" element={<AddRole />}/>
